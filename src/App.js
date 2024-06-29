706be92b51e656d8b3aeea43715aeb4d41783ea4
Jobayerdl9 <jobayerdl9@gmail.com>
2024-06-29 10:32:48 +0600

    login updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Dashbaord from "./Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./componenets/Register";
+import Login from "./componenets/Login";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashbaord />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/componenets/Login.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="flex justify-center backgroundShorten items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-lg shadow-md">
+        <h2 className="text-center text-2xl font-bold text-gray-900">Login</h2>
+        <form className="space-y-6">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Email address
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            />
+          </div>
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
+              Password
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
+            />
+          </div>
+          <div>
+            <button
+              type="submit"
+              className="w-full px-4 py-2 font-medium text-white bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
