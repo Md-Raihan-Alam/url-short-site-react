5df6babfb81f9ac745d673c1e9bb61570c524bdc
Jobayerdl9 <jobayerdl9@gmail.com>
2024-06-29 10:28:38 +0600

    route update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Dashbaord from "./Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./componenets/Register";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashbaord />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
